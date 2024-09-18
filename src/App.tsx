7e21db0870db9d616d94168b7ab9272b3999088d
Avramidi Yaroslav <passenger1177@gmail.com>
2024-09-19 02:08:01 +0300

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Available } from "./components/Available/Available";
 import { Follow } from "./components/Follow/Follow";
 import { Footer } from "./components/Footer/Footer";
@@ -7,19 +8,36 @@ import { MainScreen } from "./components/MainScreen/MainScreen";
 import { News } from "./components/News/News";
 import { About } from "./components/About/About";
 import { TopBar } from "./components/TopBar/TopBar";
+import { Loader } from './components/Loader/Loader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <TopBar />
-      <MainScreen />
-      <About />
-      <News />
-      <Gallery />
-      <Available />
-      <NewsLetter />
-      <Follow />
-      <Footer />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <TopBar />
+          <MainScreen />
+          <About />
+          <News />
+          <Gallery />
+          <Available />
+          <NewsLetter />
+          <Follow />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import "./Loader.scss";
+
+export const Loader = () => {
+  return (
+    <div className="container-loader">
+      <div className="loader loader2">
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
