b0f14e977c0b96ed67fe7d9d6c305334e0d4192a
Yaroslav Avramidi <passenger1177@gmail.com>
2024-09-21 11:35:56 +0300

    try to fix heroku

--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -58,7 +58,7 @@ export const NewsLetter = () => {
       const response = await fetch("/sendmail.php", {
         method: "POST",
         headers: {
-          "Content-Type": "application/javascript",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
